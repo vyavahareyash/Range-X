42d434267628f7513f9ba922554ddabc565a25e5
Yash Vyavahare <94339743+vyavahareyash@users.noreply.github.com>
2023-01-23 14:04:01 +0530

    Created correlations folder

--- correlation.py
+++ correlation.py
@@ -1,8 +1,24 @@
 import numpy as np 
 import pandas as pd
+import os
 
-data = pd.read_csv('tables/12_30/ADANIENT_data_table.csv')
+corrExists=os.path.exists("correlations")
+if(not corrExists):
+    os.makedirs("correlations")
 
-size = data['Candle_size']
-reversal = data['tReversal']
-print(size.corr(reversal))
+timeList = os.listdir('tables')
+
+for time in timeList:
+    corrTable = pd.DataFrame()
+    companyList = os.listdir('tables/'+time)
+    corrTable['company']=np.array(companyList)
+    corrList=[]
+    for company in companyList:
+        data = pd.read_csv('tables/{}/{}'.format(time,company))
+        size = data['Candle_size']
+        reversal = data['tReversal']
+        corrList.append(size.corr(reversal))
+    corrTable['correlation']=np.array(corrList)
+    corrTable.to_csv('correlations/{}_corr.csv'.format(time),index=False)
+        
+        
