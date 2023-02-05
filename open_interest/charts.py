13fc6906ebd789908d57a08955419f8a7602a9f2
Yash Vyavahare <94339743+vyavahareyash@users.noreply.github.com>
2023-02-05 14:49:41 +0530

    updated charts.py

--- open_interest/charts.py
+++ open_interest/charts.py
@@ -14,17 +14,27 @@ chartExists=os.path.exists("charts")
 if(not chartExists):
     os.makedirs("charts")
     
-company_list = os.listdir('oi_data')
+company_list = os.listdir('../../../Python notebooks/YT_Sandbox/Yash/send')
 
 for company in company_list:
     try:
-        data = pd.read_csv("oi_data/{}".format(company))
-        n = len(data)
-        data.drop(np.arange(n-5,n),inplace=True)
-        x=data[data['DeliveryFlag']>0]
-        plt.scatter(data['Date'],data['PerDelivery'],color='blue')
-        plt.scatter(x['Date'],x['PerDelivery'],color='red')
-        plt.savefig('charts/{}.png'.format(company))
+        data = pd.read_csv("oi_data_2/{}".format(company))
+        # n = len(data)
+        # data.drop(np.arange(n-5,n),inplace=True)
+        # x=data[data['DeliveryFlag']>0]
+        # plt.scatter(data['Date'],data['PerDelivery'],color='blue')
+        # plt.scatter(x['Date'],x['PerDelivery'],color='red')
+        # plt.savefig('charts/{}.png'.format(company))
+
+        # plt.ylim([100000,1000000000])
+        plt.plot(data['Date'],data['OI_Combined'])
+        plt.savefig('charts/OI_{}.png'.format(company))
+        plt.clf()
+        # plt.ylim([0,10000])
+        plt.plot(data['Date'],data['VWAP'])
+        plt.savefig('charts/VWAP_{}.png'.format(company))
+        plt.clf()
         
     except Exception as e:
+        plt.clf()
         print(str(e))
