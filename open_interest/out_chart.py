b3e20194787917c1e00ede99af0efb8db93d5c92
Yash Vyavahare <94339743+vyavahareyash@users.noreply.github.com>
2023-02-05 18:58:18 +0530

    Create out_chart.py

---
+++ open_interest/out_chart.py
@@ -0,0 +1,49 @@
+import os
+os.system("pip install pandas")
+os.system("pip install numpy")
+os.system("pip install matlplotlib")
+os.system("pip install seaborn")
+os.system("cls")
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+chartExists=os.path.exists("out_charts")
+if(not chartExists):
+    os.makedirs("out_charts")
+    
+dataExists=os.path.exists("flags")
+if(dataExists):
+    company_list = os.listdir('flags')
+else:
+    exit('Data does not exists')
+
+for company in company_list:
+    try:
+        data = pd.read_csv("flags/{}".format(company))
+        n = len(data)
+        data.drop(np.arange(n-5,n),inplace=True)
+        
+        x=data[abs(data['DeliveryFlag'])>0]
+        plt.scatter(data['Date'],data['PerDelivery'],color='blue')
+        plt.scatter(x['Date'],x['PerDelivery'],color='red')
+        plt.savefig('out_charts/Delivery_{}.png'.format(company))
+        plt.clf()
+
+        x=data[abs(data['OIFlag'])>0]
+        plt.scatter(data['Date'],data['Change_in_OI'],color='blue')
+        plt.scatter(x['Date'],x['Change_in_OI'],color='red')
+        plt.savefig('out_charts/OI_{}.png'.format(company))
+        plt.clf()
+        
+        x=data[abs(data['VWAPFlag'])>0]
+        plt.scatter(data['Date'],data['Change_in_VWAP'],color='blue')
+        plt.scatter(x['Date'],x['Change_in_VWAP'],color='red')
+        plt.savefig('out_charts/VWAP_{}.png'.format(company))
+        plt.clf()
+        
+    except Exception as e:
+        plt.clf()
+        print(str(e))
